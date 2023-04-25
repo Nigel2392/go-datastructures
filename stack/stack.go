33790ef98da36419f9dc08b9fa3625892d8d8c6b
Nigel Van Keulen <pvpbeast@outlook.com>
2023-04-25 15:37:46 +0200

    Added a pop-waiter for the stack, which waits for a specified duration for a variable to be available

--- stack/stack.go
+++ stack/stack.go
@@ -1,12 +1,16 @@
 package stack
 
-import "github.com/Nigel2392/go-datastructures/linkedlist"
+import (
+	"time"
+
+	"github.com/Nigel2392/go-datastructures/linkedlist"
+)
 
 // Stack is a stack data structure
 //
-// It is implemeneted using a singly linked list
+// # It is implemeneted using a singly linked list
 //
-// This is to save space, as the stack only needs to know about the top element
+// # This is to save space, as the stack only needs to know about the top element
 //
 // This does mean that the data gets prepended at the start, and then shifted off,
 // as opposed to appended and then popped off.
@@ -28,7 +32,7 @@ func (s *Stack[T]) Pop() T {
 
 // PopOK removes a value from the top of the stack
 //
-// This is the same as Shift in a linked list
+// # This is the same as Shift in a linked list
 //
 // It returns the value and a boolean indicating whether the value was removed
 func (s *Stack[T]) PopOK() (value T, ok bool) {
@@ -36,6 +40,33 @@ func (s *Stack[T]) PopOK() (value T, ok bool) {
 		return
 	}
 	return (*linkedlist.Singly[T])(s).Shift(), true
+}
+
+// PopOKWaiter returns a channel where the value will be sent when it is available
+//
+// This is the same as ShiftWaiter in a linked list
+func (s *Stack[T]) PopOKWaiter(deadline time.Duration) (ret <-chan T, ok <-chan time.Time) {
+	var deadlineWaiter = time.After(deadline)
+	var c = make(chan T, 1)
+
+	go func() {
+		for {
+			select {
+			case <-deadlineWaiter:
+				close(c)
+				return
+			default:
+				if s.Len() > 0 {
+					c <- (*linkedlist.Singly[T])(s).Shift()
+					close(c)
+					return
+				}
+			}
+		}
+	}()
+
+	return c, deadlineWaiter
+
 }
 
 // Peek returns the value at the top of the stack
